4f9a7ca9f5a56aae63f1a67861ee205f346503eb
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-03 10:58:41 -0300

    Cria a página Home :house:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div>Be Mobile App</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className='home-wrapper-container'>Home Page</div>;
+};
+
+export default Home;
